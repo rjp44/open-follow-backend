9b3829c62536b7b6aa307ce02879f348228baf43
Rob Pickering <rob@pickering.org>
2022-11-19 00:54:20 +0000

    more debug

--- src/handlers/mastodon.js
+++ src/handlers/mastodon.js
@@ -230,6 +230,7 @@ async function passthru(req, res) {
     });
     console.log('headers', { result });
     result.headers.link && res.set(result.headers);
+    await storage.save(`DEBUG-passthru-${new Date().valueOf()}`, JSON.stringify({res, result},null,2));
     console.log('sedning', { res, status: result.status });
     res.status(result.status).json(result.data);
 
