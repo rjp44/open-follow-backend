99557f35f7b639d34e7b0095b1a45ee09f999c83
Rob Pickering <rob@pickering.org>
2022-11-19 01:23:49 +0000

    Remove un-needed diagnostics from production

--- src/handlers/mastodon.js
+++ src/handlers/mastodon.js
@@ -203,7 +203,6 @@ async function passthru(req, res) {
   let { baseUrl, originalUrl, method, protocol, body } = req;
   let url = originalUrl.slice(baseUrl.length);
 
-  console.log('PASSTHRU', { url });
   try {
     if (!token || state != 'showtime')
       throw new Error(`bad auth ${state} ${token && token.length} ${uid}, ${host}`);
@@ -228,10 +227,7 @@ async function passthru(req, res) {
       method, url: `${protocol}://${host}${url}`, body,
       headers: { "Authorization": `${token.token_type} ${token.access_token}` }
     });
-    console.log('headers', { result });
     result.headers.link && res.set('Link', result.headers.link);
-    await storage.save(`DEBUG-passthru-${new Date().valueOf()}`, JSON.stringify({res:{headers: res.headers, data:res.data}, result:{headers:result.headers, data: result.data}},null,2));
-    console.log('sedning', { res, status: result.status });
     res.status(result.status).json(result.data);
 
   }
--- src/index.js
+++ src/index.js
@@ -5,19 +5,6 @@ const { exec } = require("child_process");
 const PORT = config.get('port');
 
 
-console.log(config.get('storage.url'));
-exec("ls -la credentials", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
-
 server.listen(PORT, () => console.info(`Listening on http://localhost:${PORT}`));
 
 process.on("unhandledRejection", (error) => {
