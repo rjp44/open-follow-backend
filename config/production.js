f50f918b7a716d95793ec90db1d02b5730b4d68b
Rob Pickering <rob@pickering.org>
2022-11-21 21:40:40 +0000

    Adjust host for prod

--- config/production.js
+++ config/production.js
@@ -1,4 +1,6 @@
 module.exports = {
+  backend_method: 'https',
+  backend_host: process.env.BACKEND_HOST || 'openfollow.me',
   backend_host: 'openfollow.me',
   storage: {
     url: 'gcs://credentials/cloudCredentials:openfollow@persist-openfollow'
